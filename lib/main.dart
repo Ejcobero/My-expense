33fd1abde842de8ead6bdcdd322c5a52924c91f6
Ejcobero <ejcobero@gmail.com>
2020-06-27 20:39:12 +0800

    added an input fields

--- lib/main.dart
+++ lib/main.dart
@@ -56,9 +56,19 @@ class MyHomePage extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(10),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
-                  TextField(decoration: InputDecoration(labelText: 'Title'),),
-                  TextField(decoration: InputDecoration(labelText: 'Amount'),),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    textColor: Colors.blue,
+                    onPressed: () {},
+                  )
                 ],
               ),
             ),
